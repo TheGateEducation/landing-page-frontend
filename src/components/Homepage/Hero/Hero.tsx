8e1d7959b1db2fb9c3f20e0e095669bc66d00a78
jrauljperez07 <jrauljperez02.dev@gmail.com>
2024-06-27 00:26:10 -0600

    feat: include SEO meta tags

--- src/components/Homepage/Hero/Hero.tsx
+++ src/components/Homepage/Hero/Hero.tsx
@@ -4,7 +4,7 @@ import { Navbar } from '@src/components/Navbar/Navbar'
 const Hero = () => {
     return (
         <div className="relative flex items-center justify-center h-screen bg-cover bg-center" style={{ backgroundImage: "url(https://images.pexels.com/photos/1430677/pexels-photo-1430677.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2)" }}>
-            <div className="absolute top-0 left-0 right-0 bg-white bg-opacity-60">
+            <div className="fixed top-0 left-0 right-0 bg-white bg-opacity-60 shadow-lg z-50">
                 <Navbar />
             </div>
             <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white bg-black bg-opacity-50 px-4">
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,44 @@
-import { Inter } from "next/font/google";
-import { Footer } from "@src/components/Footer/Footer";
+import React from 'react';
+import { Inter } from 'next/font/google';
+import Head from 'next/head';
+import { Footer } from '@src/components/Footer/Footer';
 
-import Hero from "@src/components/Homepage/Hero/Hero";
-import AboutUs from "@src/components/Homepage/AboutUs/AboutUs";
-import Stadistics from "@src/components/Homepage/Stadistics/Stadistics";
-import { Safeway } from "@src/components/Homepage/SafeWay/Safeway";
+import Hero from '@src/components/Homepage/Hero/Hero';
+import AboutUs from '@src/components/Homepage/AboutUs/AboutUs';
+import Stadistics from '@src/components/Homepage/Stadistics/Stadistics';
+import { Safeway } from '@src/components/Homepage/SafeWay/Safeway';
 
-
-
-const inter = Inter({ subsets: ["latin"] });
-
+const inter = Inter({ subsets: ['latin'] });
 
 const Home = () => {
     return (
-        <main>
-            <Hero />
-            <AboutUs />
-            <Stadistics/>
-            <Safeway/>
-            
+        <>
+            <Head>
+                <title>The Gate Education</title>
+                <meta name="description" content="Haz realidad tu sueño de educación internacional con nosotros. Ofrecemos programas en más de 10 países y más de 100 instituciones educacionales." />
+                <meta name="keywords" content="educación internacional, estudiar en el extranjero, programas educacionales, instituciones educacionales" />
+                <meta name="author" content="The Gate Education" />
+                <meta property="og:title" content="Tu Plataforma de Educación Internacional" />
+                <meta property="og:description" content="Haz realidad tu sueño de educación internacional con nosotros. Ofrecemos programas en más de 10 países y más de 100 instituciones educacionales." />
+                <meta property="og:image" content="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRLArqt49pngG4Dpo29bskouB_YD8MSJ2krwGKhE6CwctyyTFEWp1BQk59TLOVBg3HegY&usqp=CAU" />
+                <meta property="og:url" content="URL_DE_TU_SITIO_WEB" />
+                <meta property="og:type" content="website" />
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:title" content="Tu Plataforma de Educación Internacional" />
+                <meta name="twitter:description" content="Haz realidad tu sueño de educación internacional con nosotros. Ofrecemos programas en más de 10 países y más de 100 instituciones educacionales." />
+                <meta name="twitter:image" content="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRLArqt49pngG4Dpo29bskouB_YD8MSJ2krwGKhE6CwctyyTFEWp1BQk59TLOVBg3HegY&usqp=CAU" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main className={inter.className}>
+                <Hero />
+                <AboutUs />
+                <Stadistics />
+                <Safeway />
+                <Footer />
+            </main>
+        </>
+    );
+};
 
-
-
-            <Footer />
-        </main>
-    )
-}
-
-export default Home
+export default Home;
