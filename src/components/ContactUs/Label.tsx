23c4ceadab559f78555013d206e23e9b5ab50a55
danaxsax <dana.salinas.tareas@gmail.com>
2024-07-18 08:24:15 -0600

    feat: form

---
+++ src/components/ContactUs/Label.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+interface InputFieldProps {
+    children: React.ReactNode;
+    type: string;
+    placeholder: string;
+}
+
+const InputField: React.FC<InputFieldProps> = ({ children, type, placeholder }) => {
+    return (
+        <label className="block mb-4">
+            <span className="block text-lg font-medium text-gray-700">{children}</span>
+            <input
+                type={type}
+                placeholder={placeholder}
+                className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400 focus:outline-none focus:border-purple-900 focus:ring-2 focus:ring-customPurple"
+            />
+        </label>
+    );
+};
+
+export default InputField;
